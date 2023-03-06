f5e40514c9921944acd217514014e450cb410c2b
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-06 13:00:41 -0800

    recieving cost and name to form

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,4 +1,53 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, changeCost } from "../store";
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const { name, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      cost: state.form.cost,
+    };
+  });
+
+  const handleNameChange = (e) => {
+    dispatch(changeName(e.target.value));
+  };
+
+  const handleCostChange = (e) => {
+    const carCost = parseInt(e.target.value) || 0;
+    dispatch(changeCost(carCost));
+  };
+
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add car</h4>
+      <form action="">
+        <div className="field-group">
+          <div className="field">
+            <label htmlFor="" className="label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="" className="label">
+              Cost
+            </label>
+            <input
+              type="number"
+              className="input is-expanded"
+              value={cost || ""}
+              onChange={handleCostChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 export default CarForm;
